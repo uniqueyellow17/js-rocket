7435752c8224c0479684fee6daa3bef6b088c11b
uniqueyellow17 <97690590+uniqueyellow17@users.noreply.github.com>
2022-12-08 13:12:43 -0500

    More changes

---
+++ js/rocket.js
@@ -0,0 +1,19 @@
+var timer = null;
+var countdownNumber = 10;
+
+var changeState = function (state) {
+  document.body.className = 'body-state'+
+    state;
+
+  if (state == 2) {
+    timer = setInterval(function () {
+      document.getElementById('countdown').
+        innerHTML = countdownNumber;
+        countdownNumber = countdownNumber -1;
+        if (countdownNumber <= 0) {
+          className
+        }
+    }, 1000);
+
+  };
+}
--- js/script.js
+++ js/script.js
@@ -1,32 +1,43 @@
 var doCoolStuff = function () {
-  document.getElementById('cool').className = 'cool red';
-}
-var car.define = {
-  make: 'VW',
-  type: 'Polo',
-  color: 'blue',
-  isTurnedOn: false,
-  numberOfWheels: 4,
-  seats: [
-      'seat 1',
-      'seat 2',
-      'seat 3',
-      'seat 4'
-    ],
-  turnOn: function () {
-    this.isTurnedOn = true;
-  },
-  fly: function () {
-    alert('fly');
-  },
-  switchCar: function (isOn) {
-    console.log('turn car '+isOn);
-    if (isOn == true) {
-      this.isTurnedOn = true;
+    var currentClassNAme = document.getElementById('cool').className;
+
+    if (currentClassName == '') {
+      document.getElementById('cool').className = 'cool red';
     } else {
-      this.isTurnedOn = false;
+      document.getElementById('cool').className = 'cool';
     }
-  }
+}
+
+var sayMyName = function (name) {
+    alert('My name is: ' +name);
+}
+
+var car = {
+    make: 'VW',
+    type: 'Polo',
+    color: 'blue',
+    isTurnedOn: false,
+    numberOfWheels: 4,
+    seats: [
+        'seat 1',
+        'seat 2',
+        'seat 3',
+        'seat 4'
+      ],
+    turnOn: function () {
+      this.isTurnedOn = true;
+    },
+    fly: function () {
+      alert('fly');
+    },
+    switchCar: function (isOn) {
+      console.log('turn car '+isOn);
+      if (isOn == true) {
+        this.isTurnedOn = true;
+      } else {
+        this.isTurnedOn = false;
+      }
+    }
 };
 
 console.log("hello there friends!");
