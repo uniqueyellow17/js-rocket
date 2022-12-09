e966f1d8bf7e31122ca085026e67e67c636ca026
uniqueyellow17 <97690590+uniqueyellow17@users.noreply.github.com>
2022-12-08 21:49:59 -0500

    Made my rocket!

--- js/rocket.js
+++ js/rocket.js
@@ -4,17 +4,34 @@ var countdownNumber = 10;
 var changeState = function (state) {
   document.body.className = 'body-state'+
     state;
+    clearInterval(timer);
+    countdownNumber = 10;
+    document.getElementById('countdown').
+      innerHTML = countdownNumber;
 
   if (state == 2) {
     timer = setInterval(function () {
+      countdownNumber = countdownNumber -1;
       document.getElementById('countdown').
         innerHTML = countdownNumber;
-        countdownNumber = countdownNumber -1;
+        
         if (countdownNumber <= 0) {
-          document.body.className = 'body-state'+
-          state;
-        }
-    }, 100);
+          changeState(3);
+        };
+    }, 500);
+  } else if (state == 3) {
+    var success = setTimeout(function ()
+      {
+        var randomNumber = Math.round
+          (Math.random()*10);
 
+        console.log('randomNumber:',
+          randomNumber)
+        if (randomNumber > 5) {
+          changeState(4);
+        } else {
+          changeState(5);
+        }
+      }, 2000);
   };
 }
